c3c9dbd104c3a5b6e1d6b0ed2993c6f4a372f3ca
muhammadmichael <michaelmaulana93@gmail.com>
2022-10-05 20:42:16 +0700

    create rest for create and reply komentar

--- routes/komentar.js
+++ routes/komentar.js
@@ -8,101 +8,55 @@ const Komentar = db.komentars;
 const Op = db.Sequelize.Op;
 
 // Create (Post) Sebuah Komentar
-// GET
-// router.get('/tambah/:id', function (req, res, next) {
-//     var id = parseInt(req.params.id);
+// POST
+router.post('/tambah/:id', function (req, res, next) {
+    // Params id disini adalah id berita yang memiliki tersebut
+    var id = parseInt(req.params.id);
 
-//     Berita.findByPk(id)
-//     .then(data => {
-//         if (data) {
-//             res.render('formtambahkomentar', { 
-//                 title: 'Tambah Komentar',
-//                 berita: data
-//          });
-//         } else {
-//         // kalau data tidak ada send 404
-//         res.status(404).send({
-//             message: "Tidak ada berita dengan id= " + id
-//         })
-//         }
-//     })
-//     .catch(err => {
-//         res.json({
-//         info: "Error",
-//         message: err.message
-//         });
-//     });
-// });
+    try {
+        var komentar = {
+            text: req.body.text,
+            beritumId: id
+        }
 
-// // POST
-// router.post('/tambah/:id', function (req, res, next) {
-//     var id = parseInt(req.params.id);
+        Komentar.create(komentar)
+            .then(() => {
+                res.json({
+                    info: "Komentar Berhasil Ditambahkan ke Berita dengan Id " + id,
+                    komentar: komentar
+                });
+            });
 
-//     try { 
-//         var komentar = {
-//             text: req.body.text,
-//             beritumId: id
-//         }
+    } catch (error) {
+        console.log(error);
+        return res.send(`Error: ${error}`);
+    }
+});
 
-//         Komentar.create(komentar)
-//         .then( () => {
-//                   return res.redirect('/berita/detail/'+ id);
-//         });
+// Reply Sebuah Komentar
+// POST
+router.post('/reply/:id', function (req, res, next) {
+    // Params id disini adalah idParent komentar tersebut
+    var id = parseInt(req.params.id);
 
-//     } catch (error) {
-//         console.log(error);
-//         return res.send(`Error: ${error}`);
-//     }
-// });
+    try {
+        var komentar = {
+            text: req.body.text,
+            komentarId: id // foreign key ke parent
+        }
 
-// // Reply Sebuah Komentar
-// // GET
-// router.get('/reply/:id', function (req, res, next) { 
-//     // Params id disini adalah idParent komentar tersebut
-//     var id = parseInt(req.params.id);
+        Komentar.create(komentar)
+            .then(() => {
+                res.json({
+                    info: "Reply Berhasil Ditambahkan ke Komentar dengan Id " + id,
+                    komentar: komentar
+                });
+            });
 
-//     Komentar.findByPk(id)
-//     .then(data => {
-//         if (data) {
-//             res.render('formreplykomentar', { 
-//                 title: 'Reply Komentar',
-//                 komentar: data,
-//          });
-//         } else {
-//         // kalau data tidak ada send 404
-//         res.status(404).send({
-//             message: "Tidak ada komentar dengan id= " + id
-//         })
-//         }
-//     })
-//     .catch(err => {
-//         res.json({
-//         info: "Error",
-//         message: err.message
-//         });
-//     });
-// });
-
-// // POST
-// router.post('/reply/:id', function (req, res, next) {
-//     // Params id disini adalah idParent komentar tersebut
-//     var id = parseInt(req.params.id);
-
-//     try { 
-//         var komentar = {
-//             text: req.body.text,
-//             komentarId: id // foreign key ke parent
-//         }
-
-//         Komentar.create(komentar)
-//         .then( () => {
-//                   return res.redirect('/');
-//         });
-
-//     } catch (error) {
-//         console.log(error);
-//         return res.send(`Error: ${error}`);
-//     }
-// });
+    } catch (error) {
+        console.log(error);
+        return res.send(`Error: ${error}`);
+    }
+});
 
 module.exports = router;
